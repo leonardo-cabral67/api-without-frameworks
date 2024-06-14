81de5147c649bd0725a4ce2fb78d597aa15e4935
Leonardo <leonardoleocabral@outlook.com>
2024-06-14 01:40:07 -0300

    ✨ create route `/` and `/api/movies/

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
 import { createServer, ServerResponse, IncomingMessage } from "node:http";
-import eventEmitter from "node:events";
-import url from "node:url";
+import { getMovies } from "./infra/database";
+
+const routes = {
+  "GET:/": (req: IncomingMessage, res: ServerResponse) => {
+    res.writeHead(200)
+    res.end(JSON.stringify({ message: "api is working" }))
+  },
+  "GET:/api/movies": (req: IncomingMessage, res: ServerResponse) => {
+    const movies = getMovies()
+    res.writeHead(200)
+    res.end(JSON.stringify(movies))
+  }
+}
 
 const httpHandler = async (req: IncomingMessage, res: ServerResponse) => {
   res.setHeader("Content-type", "application/json");
   res.setHeader("Keep-alive", "timeout=1");
-  res.writeHead(200);
-  res.end(JSON.stringify({ message: "hello world" }));
+  const [httpMethod, path] = [req.method, req.url]
+  const route: string = `${httpMethod}:${path}`
+  if (!routes[route as keyof typeof routes]) return res
+    .end(JSON.stringify({ page_not_found: "this page was not found" }))
+    .writeHead(404)
+
+  routes[route as keyof typeof routes](req, res)
 };
 
 const app = createServer(httpHandler);
 const PORT = 3000;
 app
-  .listen(PORT)
-  .on("listening", () => console.log("🚀 app running in port", PORT));
+  .listen(PORT, () => console.log('🚀 app running in port 3000'))
